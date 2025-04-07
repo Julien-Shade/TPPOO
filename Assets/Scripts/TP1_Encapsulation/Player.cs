a50d5cea5bcb95ecacbb9a89998dfa9f102c3d8c
Julien-Shade <j.promi@rubika-edu.re>
2025-04-07 15:53:40 +0400

    Exercice_POO

---
+++ Assets/Scripts/TP1_Encapsulation/Player.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class Player : MonoBehaviour {
+
+    // variables du player
+    [SerializeField]
+    private string playerName;
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private int maxHealth;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private int gold;
+    [SerializeField]
+    private bool isInvincible;
+
+    [SerializeField]
+    private Transform transformPlayer;
+
+
+    
+
+
+
+    // accesseurs
+    public string PlayerName { get => playerName; set => playerName = value; }
+    //public int Health { get => health; set => health = value; }
+    public int Health
+    {
+        get
+        {
+            Debug.Log("get health");
+            health = Mathf.Clamp(this.health, 0, maxHealth);
+            return this.health;
+        }
+        set
+        {
+            Debug.Log("value " + value);
+            Debug.Log("set  health " + health);
+            health = Mathf.Clamp(value, 0, maxHealth);
+            Debug.Log("set health après clamp " + health);
+        }
+    }
+
+    public int MaxHealth { get => maxHealth; set => maxHealth = value; }
+    public float MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        } 
+        set
+        {
+            moveSpeed = Mathf.Clamp(value, -10, 10); // je clamp la vitesse entre -10 et 10
+            moveSpeed = value;
+        } 
+    
+    }
+    public int Gold { get => gold; set => gold = value; }
+    public bool IsInvincible { get => isInvincible; set => isInvincible = value; }
+    public Transform TransformPlayer { get => transformPlayer; set => transformPlayer = value; }
+
+    /*    public Transform Transform
+{
+get
+{
+this.Transform = transformPlayer;
+}
+set
+{
+transformPlayer;
+}
+}*/
+
+    public void GainGold(int amount)
+    {
+        gold += amount;
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        if (isInvincible) return;
+
+
+        Health -= amount;
+    }
+
+    public void Heal(int amount)
+    {
+        Health += amount;
+    }
+
+    public void Deplacement(Vector3 pos)
+    {
+        TransformPlayer.Translate(pos * moveSpeed * Time.deltaTime);
+    }
+}
+
--- Assets/Scripts/TP1_Encapsulation/PlayerCharacterBroken.cs
+++ Assets/Scripts/TP1_Encapsulation/PlayerCharacterBroken.cs
@@ -5,27 +5,57 @@ namespace TP1_Encapsulation
     public class PlayerCharacterBroken : MonoBehaviour
     {
         // Toutes les données sont publiques et peuvent être modifiées n'importe où
-        public string playerName;
-        public int health;
-        public int maxHealth;
-        public float moveSpeed;
-        public int gold;
-        public bool isInvincible;
-        
+        /*        public string playerName;
+                public int health;
+                public int maxHealth;
+                public float moveSpeed;
+                public int gold;
+                public bool isInvincible;
+                */
+        [SerializeField]
+        Transform pos;
+        [SerializeField]
+        Player player;
+
+
+
+
+
+
+        private void Start()
+        {
+            pos = transform;
+
+            player = gameObject.AddComponent<Player>();
+            player.MaxHealth = 100;
+            player.Health = 100;
+            //player.TakeDamage(105);
+            Debug.Log("health = " + player.Health);
+            player.TransformPlayer = pos;
+            player.Deplacement(pos.transform.forward);
+        }
         void Update()
         {
+
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                player.Deplacement(pos.transform.forward);
+            }
             
-            if (health <= 0)
+            /*if (health <= 0)
             {
                 Debug.Log("Player is dead");
-            }
+            }*/
 
             // La vitesse peut être modifiée à n'importe quelle valeur
             // Le personnage peut avoir une vitesse négative car rien ne l'empêche
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            
+            
+            //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
         
-        public void GainGold(int amount)
+        /*public void GainGold(int amount)
         {
             gold += amount;
         }
@@ -34,6 +64,6 @@ namespace TP1_Encapsulation
         public void TakeDamage(int amount)
         {
             health -= amount;
-        }
+        }*/
     }
 }
